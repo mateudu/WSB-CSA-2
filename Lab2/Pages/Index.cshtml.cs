cd7efa8140cc342bb06d5e4af5bb64ec0be96763
Mateusz Dudek <mateudu@outlook.com>
2022-11-01 10:55:58 +0100

    Update package version

--- Lab2/Pages/Index.cshtml.cs
+++ Lab2/Pages/Index.cshtml.cs
@@ -5,17 +5,20 @@ using System.Collections.Generic;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Core;
+using Microsoft.Extensions.Configuration;
 
 namespace Lab2.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
         public IList<KeyValuePair<string, string>> Secrets { get; } = new List<KeyValuePair<string, string>>();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
         {
-            _logger = logger;
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public void OnGet()
@@ -30,7 +33,14 @@ namespace Lab2.Pages
                     Mode = RetryMode.Exponential
                 }
             };
-            var client = new SecretClient(new Uri(Environment.GetEnvironmentVariable("KeyVault") ?? throw new InvalidOperationException("KeyVault URI not set in configuration")), new DefaultAzureCredential(), options);
+
+            var keyVaultUri = _configuration["KeyVault"];
+            if (string.IsNullOrWhiteSpace(keyVaultUri))
+            {
+                throw new ArgumentException("KeyVault URI not set in configuration");
+            }
+
+            var client = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential(), options);
 
             foreach (var secretProperties in client.GetPropertiesOfSecrets())
             {
